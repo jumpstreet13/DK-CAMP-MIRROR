19ef57d8d5fbee3510071a6af7aa0c0dd7b9d206
abakarmagomedov <skat131998@mail.ru>
2017-02-24 13:06:02 +0300

    some work

--- app/src/main/java/com/smedialink/abakarmagomedov/dk_camp_mirror/FordeleActivity.java
+++ app/src/main/java/com/smedialink/abakarmagomedov/dk_camp_mirror/FordeleActivity.java
@@ -2,12 +2,36 @@ package com.smedialink.abakarmagomedov.dk_camp_mirror;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import butterknife.BindView;
+
 
 public class FordeleActivity extends AppCompatActivity {
+
+    @BindView(R.id.toolBarInActivityFordele) Toolbar mToolbar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fordele);
+        mToolbar = (Toolbar) findViewById(R.id.toolBarInActivityFordele);
+        mToolbar.setTitle("");
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.toolbar_menu, menu);
+        MenuItem item = menu.findItem(R.id.menu);
+        item.setActionView(R.layout.menu_item);
+        return true;
     }
 }
--- app/src/main/java/com/smedialink/abakarmagomedov/dk_camp_mirror/HowedMenuActivity.java
+++ app/src/main/java/com/smedialink/abakarmagomedov/dk_camp_mirror/HowedMenuActivity.java
@@ -1,20 +1,44 @@
 package com.smedialink.abakarmagomedov.dk_camp_mirror;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.ContentFrameLayout;
 import android.text.SpannableString;
 import android.text.Spanned;
 import android.text.style.UnderlineSpan;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
+
+import butterknife.BindView;
+import butterknife.OnClick;
+
 
 public class HowedMenuActivity extends AppCompatActivity {
 
-    private Button button;
+ /*   @BindView(R.id.buttonMinProfil) Button mProfilButton;
+
+    @OnClick(R.id.buttonMinProfil)
+    void onClick() {
+        Log.d("Butter", "clicked");
+        Intent intent = new Intent(HowedMenuActivity.this, FordeleActivity.class);
+        startActivity(intent);
+    }*/
+    private Button mButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_howed_menu);
+        mButton = (Button) findViewById(R.id.buttonMinProfil);
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HowedMenuActivity.this, FordeleActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
